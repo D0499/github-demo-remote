0d0c5ff9399001f6f2117f691a590b3b9a128a91
D0499 <75064190+D0499@users.noreply.github.com>
2022-02-02 17:33:09 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,14 @@
+# Add implementation
+def add(x,y):
+    pass
+	
+# Sub implementation
+def subtract(x,y):
+    return x-y       # on master branch
+    
+#multiply implementation    
+def multiply(x,y):
+    return x*y   # on branch456
+    
+def divide(x,y):
+    pass
